1294927461d7db869a16dfed1617644d66d3551b
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-01 19:15:42 +0700

    du lieu

--- a.js
+++ a.js
@@ -1,6 +1,15 @@
-console.log("hello"); 
-console.log("hello");
-console.log("hello"); 
-console.log("xin chao"); 
-
-// ctrl + c
+// var let const
+//null co tinh gan la null
+// var a  = null;
+// console.log(a);
+//undefined
+// Dang 1
+// var a;
+//Dang 2 3
+// function foo(){
+//     return;
+// }
+// console.log(foo());
+//Dang 4
+// const ti = {name :'Ti'}
+// console.log(ti.tuoi);

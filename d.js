f51e81522a988bb4d32ee073e880fd0ab397e1c7
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-05 17:39:40 +0700

    a

---
+++ d.js
@@ -0,0 +1,8 @@
+const numbers = [1,3,2,4,6,5,8,7,9];
+
+const persons = [
+    {name : 'Ti' , age : 10 , height : 120},
+    {name : 'Teo' , age : 12 , height : 110},
+    {name : 'Tun' , age : 14 , height : 130},
+    {name : 'Hoa' , age : 11 , height : 100},
+]
